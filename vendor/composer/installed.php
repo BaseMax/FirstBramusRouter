5e67d691c4e83cc87797c09837ec344caa5385fa
Max Base <MaxBaseCode@gmail.com>
2021-12-15 18:39:39 +0330

    Upload example files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'dfb47d7e6e772d6dd0effc85afbb18989a8cbae3',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'dfb47d7e6e772d6dd0effc85afbb18989a8cbae3',
+            'dev_requirement' => false,
+        ),
+        'bramus/router' => array(
+            'pretty_version' => '1.6.1',
+            'version' => '1.6.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bramus/router',
+            'aliases' => array(),
+            'reference' => '55657b76da8a0a509250fb55b9dd24e1aa237eba',
+            'dev_requirement' => false,
+        ),
+    ),
+);
