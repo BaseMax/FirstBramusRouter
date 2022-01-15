a53a291d6e3da0066764856bf263b73dd33dcb86
Max Base <MaxBaseCode@Gmail.Com>
2022-01-15 10:55:27 +0330

    Create without-composer.php

---
+++ without-composer.php
@@ -0,0 +1,24 @@
+<?php
+// Require composer autoloader
+require __DIR__ . '/Router.php';
+
+// Create Router instance
+$router = new \Bramus\Router\Router();
+
+// Define routes
+$router->get('/', function() {
+    print "Hello, World!\n";
+});
+$router->get('/([0-9]+)-([a-z-]+).html', function($digit, $name) {
+    print "$digit, $name\n";
+});
+
+// $router->get('pattern', function() { /* ... */ });
+// $router->post('pattern', function() { /* ... */ });
+// $router->put('pattern', function() { /* ... */ });
+// $router->delete('pattern', function() { /* ... */ });
+// $router->options('pattern', function() { /* ... */ });
+// $router->patch('pattern', function() { /* ... */ });
+
+// Run it!
+$router->run();
